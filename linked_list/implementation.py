e64fec46f46aae661ae2b76ae2c6a7bd4d7a6639
Trevor Mountney <trevormountney@gmail.com>
2017-04-21 01:51:18 +0000

    Initial commit

--- linked_list/implementation.py
+++ linked_list/implementation.py
@@ -1,4 +1,7 @@
-from .interface import AbstractLinkedList
+#from .interface import AbstractLinkedList
+#from .node import Node
+from interface import AbstractLinkedList
+from node import Node
 
 
 class LinkedList(AbstractLinkedList):
@@ -7,34 +10,154 @@ class LinkedList(AbstractLinkedList):
     """
 
     def __init__(self, elements=None):
-        pass
+        self.start = None
+        self.end = None
+        
+        if elements:
+            for elem in elements:
+                self.append(elem)
+        self.next_node = self.start
 
     def __str__(self):
-        pass
+        return str([each.elem for each in self])
 
     def __len__(self):
-        pass
+        return sum((1 for each in self))
 
     def __iter__(self):
-        pass
-
+        '''code if not using next (not tested in all situations) - now obsolete
+        
+        next_node = self.start.next
+        node_list = [self.start] # are lists allowed in this project?
+        if next_node:
+            while next_node != self.end:
+                node_list.append(next_node)
+                next_node = next_node.next
+            else:
+                node_list.append(self.end)
+        return iter(node_list)
+        '''
+        '''if self.start == self.end:
+            return iter(self.start)
+        else:'''
+        self.next_node = self.start
+        return self
+        
+        
     def __getitem__(self, index):
-        pass
+        for i, v in enumerate(self):
+            if i == index:
+                return v.elem
+            #else:
+            #    raise IndexError
 
     def __add__(self, other):
-        pass
+        new_linked_list = LinkedList()
+        for each in self:
+            new_linked_list.append(each)
+        for each in other:
+            new_linked_list.append(each)
+        return new_linked_list
 
     def __iadd__(self, other):
         pass
 
     def __eq__(self, other):
-        pass
+        self_list = [each for each in enumerate(self)]
+        other_list = [each for each in enumerate(other)]
+        if self_list == other_list:
+            return True
+            
 
     def append(self, elem):
-        pass
+        node = Node(elem)
+        
+        if self.start is None:
+            self.start = node
+          
+        elif self.end is None:
+            self.start.next = node
+            self.end = node
+        else:
+            self.end.next = node
+            self.end = node
 
     def count(self):
-        pass
+        return len(self)
 
     def pop(self, index=None):
-        pass
+        if index == 0:
+            return self.start.elem
+        
+        if index is None:
+            index = len(self) - 1
+        
+        count = 0
+        previous = None
+        current = self.start
+        
+        while index != count:
+            previous = current
+            current = current.next
+            count += 1
+        else:
+            return current.elem
+            
+        #return self[index]
+
+    
+    
+    def next(self):
+        try: 
+            if self.next_node is None:
+                raise StopIteration
+            else:
+                result = self.next_node
+                self.next_node = self.next_node.next
+                return result
+        except(AttributeError):
+            raise StopIteration
+
+        '''try:
+            while self.next_node != self.end:
+                yield self.next_node
+                self.next_node = self.next_node.next
+        except (AttributeError):
+            raise StopIteration'''
+            
+    
+
+something = LinkedList([5, 6, 7])
+
+'''print(something.start)
+print(something.start.next)
+print(something.end)
+print(something.end.next)'''
+
+for each in something:
+    print(each)
+    
+for each in something:
+    print(each)
+    
+print(len(something))
+print(len(something))
+print(something.count())
+print(something)
+
+print(enumerate(iter(something)) == enumerate(something))
+print(something[1])
+
+something2 = LinkedList([5, 6, 7])
+
+print(something + something2)
+
+my_list = LinkedList()
+new_list = my_list + LinkedList([1])
+#print(new_list == my_list)
+
+print(LinkedList([1] == LinkedList([1])))
+
+
+print(something2.pop())
+
--- linked_list/node.py
+++ linked_list/node.py
@@ -4,13 +4,29 @@ class Node(object):
     """
 
     def __init__(self, elem, next=None):
-        pass
+        self.elem = elem
+        self.next = next # we are passing a node object for next
 
     def __str__(self):
-        pass
+        if self.next:
+            return "Node({}) > Node({})".format(self.elem, self.next.elem)
+        else:
+            return "Node({}) > /".format(self.elem)
+            
+    def __iter__(self):
+        return iter([self.elem])
 
     def __eq__(self, other):
-        pass
+        try:
+            return all([self.elem == other.elem, self.next == other.next])
+        #return self.elem == other.elem and self.next == other.next
+        except(AttributeError):
+            return False
+            
+   # def __ne__(self, other): # is this needed?
+    #    return self.elem != other.elem and self.next != other.next
 
     def __repr__(self):
-        pass
+        return self.__str__()
+
+
